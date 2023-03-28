5366517fdc3a6ec9e94d531de2b3d32ecad1b151
Hans <classsickpeople@gmail.com>
2023-03-28 11:18:36 +0800

    Setup vite, eslint, react and typescript

--- README.md
+++
@@ -1,9 +0,0 @@
-# Tic-Tac-Toe
-
-Just a simple Tic-Tac-Toe Game
-Mode Player vs Player Only
-
-Build with:
-- HTML
-- CSS
-- JavaScript
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from 'react'
+
+const App = (): ReactElement => {
+    return (
+        <div className="App">
+            <p>App</p>
+        </div>
+    )
+}
+
+export default App
