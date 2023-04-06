416a8bde30f07ccc1be5ad87c9a663fc545acd22
Hans <classsickpeople@gmail.com>
2023-04-06 11:00:24 +0800

    Update reset button

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -4,23 +4,24 @@ import { MdOutlineRestartAlt } from 'react-icons/md'
 
 interface IBoardProps {
     isGameStarted: boolean
+    values: number[]
     startTheGame: () => void
     history: string[]
 }
 
-const ToolBar = ({ isGameStarted, startTheGame, history }: IBoardProps): ReactElement => {
+const ToolBar = ({ isGameStarted, values, startTheGame, history }: IBoardProps): ReactElement => {
     return (
-        <div className='fixed flex left-0 bottom-0 w-screen items-center justify-evenly pb-4'>
-            <button className={`${isGameStarted ? 'opacity-100' : 'opacity-0'} flex flex-col transition-fast`}>
+        <div className='fixed flex left-0 bottom-0 w-screen items-center justify-center gap-x-8 pb-4'>
+            <button onClick={() => { startTheGame() }} className={`${isGameStarted && values.includes(1) ? 'opacity-100 h-fit' : 'opacity-0 h-0'} w-12 text-center transition-fast`}>
                 <MdOutlineRestartAlt className='w-8 h-8 mx-auto' />
                 <span>Reset</span>
             </button>
-            <button onClick={() => { startTheGame() }} className={`${isGameStarted ? 'opacity-0' : 'opacity-100 border'} border-orange rounded-lg h-fit px-4 py-2 transition-fast`}>
+            <button onClick={() => { startTheGame() }} className={`${isGameStarted ? 'opacity-0 h-0' : 'opacity-100 h-fit border'} border-orange rounded-lg px-4 py-2 transition-fast`}>
                 {
                     history.length > 0 ? 'Play Again' : 'Start The Game!'
                 }
             </button>
-            <button className='flex flex-col'>
+            <button className='text-center w-12'>
                 <TbHistory className='w-8 h-8 mx-auto' />
                 <span>History</span>
             </button>
